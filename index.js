ca1e63ff904f50d38e99bc9cbcb248bb95e4a3d3
Tim Tawan <tim@tawan.me>
2019-11-25 20:41:20 +1100

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/** @format */
+import 'react-native-gesture-handler';
+
+import {AppRegistry} from 'react-native';
+import Entrypoint from './app/Entrypoint';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => Entrypoint);
